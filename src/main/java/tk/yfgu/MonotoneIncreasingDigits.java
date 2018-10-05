0350234989c362597c2f71e0d1669b54e38fd581
Yifan Gu <YFGu0618@outlook.com>
2018-10-05 03:37:09 -0400

    Solution for 402, 718.

---
+++ src/main/java/tk/yfgu/MonotoneIncreasingDigits.java
@@ -0,0 +1,26 @@
+package tk.yfgu;
+
+/**
+ * LeetCode Solution No.0738.
+ *
+ * @see <a href="MonotoneIncreasingDigits"> https://leetcode.com/problems/monotone-increasing-digits
+ *      </a>
+ * @author Yifan Gu
+ */
+
+public class MonotoneIncreasingDigits {
+  public static int monotoneIncreasingDigits(int N) {
+    StringBuilder num = new StringBuilder(Integer.toString(N));
+    int tail = num.length();
+    for (int i = num.length() - 1; i > 0; i--) {
+      if (num.charAt(i) < num.charAt(i - 1)) {
+        tail = i;
+        num.setCharAt(i - 1, (char) (num.charAt(i - 1) - 1));
+      }
+    }
+    for (int i = tail; i < num.length(); i++) {
+      num.setCharAt(i, '9');
+    }
+    return Integer.valueOf(num.toString());
+  }
+}
---
+++ src/main/java/tk/yfgu/RemoveKDigits.java
@@ -0,0 +1,40 @@
+package tk.yfgu;
+
+/**
+ * LeetCode Solution No.0402.
+ *
+ * @see <a href="RemoveKDigits"> https://leetcode.com/problems/remove-k-digits </a>
+ * @author Yifan Gu
+ */
+
+public class RemoveKDigits {
+  public static String removeKdigits(String num, int k) {
+    int len = num.length();
+    if (k <= 0) {
+      return num;
+    } else if (k >= len) {
+      return "0";
+    }
+    // Use char array to simulate a stack with random access
+    char[] stack = new char[len];
+    int idxTop = 0;
+    int leftRm = k;
+    for (char ch : num.toCharArray()) {
+      while (leftRm > 0 && idxTop > 0 && ch < stack[idxTop - 1]) {
+        idxTop--;
+        leftRm--;
+      }
+      stack[idxTop++] = ch;
+    }
+    // Get rid of the leading '0's
+    int start = 0;
+    while (start < len && stack[start] == '0') {
+      start++;
+    }
+    // If after removal only left '0's, should return "0" instead of "", fix the problem
+    if (len - k - start == 0) {
+      start--;
+    }
+    return new String(stack, start, len - k - start);
+  }
+}
