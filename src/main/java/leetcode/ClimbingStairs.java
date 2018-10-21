c09e052a24f64dd200a3aa4245afba3f32ac9997
Yifan Gu <YFGu0618@outlook.com>
2018-10-21 03:55:27 -0400

    Solution for 70

---
+++ src/main/java/leetcode/ClimbingStairs.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * LeetCode Solution No.0070.
+ *
+ * @see <a href="ClimbingStairs"> https://leetcode.com/problems/climbing-stairs </a>
+ * @author Yifan Gu
+ */
+
+public class ClimbingStairs {
+  public int climbStairs(int n) {
+    if (n <= 2) {
+      return n;
+    }
+    int[] dp = new int[n];
+    dp[0] = 1;
+    dp[1] = 2;
+    for (int i = 2; i < n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n - 1];
+  }
+}
